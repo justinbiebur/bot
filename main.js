8643c9e1da19ee953c2553af225870265ba704be
debjyoti2019 <47483452+debjyoti2019@users.noreply.github.com>
2019-10-26 10:48:10 +0530

    changed keys

--- main.js
+++ main.js
@@ -1,9 +1,9 @@
 var Twit= require('twit');
 var T = new Twit({
-    consumer_key:         'vJHaz87lxjKzpK52WgNZbxJrw',
-    consumer_secret:      'KiQp1ZdPUOXIwuwQoyAJQATc8srDxcQ4KHVjwGOaq5EqKLBlo0',
-    access_token:         '1161713462956834816-9kNypjHpXJM2xi1dE1T5iGgxTRBQHl',
-    access_token_secret:  '03pjFsN44l0SidzFaanwSOGIJUeAHN0zKIleFjdDjkDec',
+    consumer_key:         'vJHaz87lxjKzpK52WgNZbgjhg',
+    consumer_secret:      'JiQp1ZdPUOXIwuwQoyAJQATc8srDxcQ4KHVjwGOaq5EqKLBlo0',
+    access_token:         '1181713462956834816-9kNypjHpXJM2xi1dE1T5iGgxTRBQHl',
+    access_token_secret:  '03pjFsN34l0SidzFaanwSOGIJUeAHN0zKIleFjdDjkDec',
     
   })
 
